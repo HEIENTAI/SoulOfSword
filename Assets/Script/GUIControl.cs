fd0dceb2d7f515e56e07614e36e1319198a74282
Ying-Yi Huang <silverwings999@gmail.com>
2013-01-29 18:02:50 +0800

    first version
    上傳初始版本

---
+++ Assets/Script/GUIControl.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class GUIControl{
+
+	// Use this for initialization
+	public GUIControl()
+	{
+			
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+}
