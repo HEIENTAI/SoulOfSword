bc2df218a1c7ee0d2fd3039b0af4f2c21d898089
Feles <billin73@gmail.com>
2013-08-19 12:28:54 +0800

    1. 刪除整個專案，之後重建

--- Client/Assets/Script/Battle.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-
-/// <summary>
-/// 處理戰鬥相關
-/// </summary>
-public class Battle
-{
-}
--- Client/Assets/Script/ParticleManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-public class ParticleManager : MonoBehaviour
-{
-
-}
